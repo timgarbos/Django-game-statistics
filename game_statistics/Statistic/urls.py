d1e8b6e987eae0be344b1134738b9cbd01efadd3
Tim garbos <timgarbos@gmail.com>
2010-10-16 22:00:29 +0200

    only post data

--- game_statistics/Statistic/urls.py
+++ game_statistics/Statistic/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 urlpatterns = patterns('game_statistics.Statistic.views',
-    (r'/postentry$', 'postentry'),
-    (r'/test$', 'test'),
+    (r'postentry$', 'postentry'),
+    (r'test$', 'test'),
 
 )
 
