3643c4c5b898d8a89b1e913b092fd3358571412e
Tim garbos <timgarbos@gmail.com>
2010-10-17 16:29:24 +0200

    fb test

--- game_statistics/Statistic/views.py
+++ game_statistics/Statistic/views.py
@@ -65,5 +65,6 @@ def fb_canvas(request,app_id,stat_name):
     friends.append({u'uid':request.facebook.uid})
     highscore = []
     for f in friends:
-        highscore.append(StatisticEntry.objects.filter(statistic=s).filter(user__uid=f.get('uid')).order_by('value')[0])
+        fbuser = StatUser.objects.get_or_create(type='facebook', uid=f.get('uid'))
+        highscore.append(StatisticEntry.objects.filter(statistic=s).filter(user=fbuser).order_by('value')[0])
     return direct_to_template(request, 'canvas.html', extra_context={'uid': request.facebook.uid,'highscore':highscore})
