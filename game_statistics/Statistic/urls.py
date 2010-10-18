b691c687ea0ecd0493c5a7325d44fe469df505bc
Tim garbos <timgarbos@gmail.com>
2010-10-18 09:52:42 +0200

    fb test

--- game_statistics/Statistic/urls.py
+++ game_statistics/Statistic/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('game_statistics.Statistic.views',
     (r'test$', 'test'),
     (r'invite$', 'invite_friends'),
     (r'fbgame/(?P<app_id>\d+)/(?P<stat_name>\w+)/$', 'fb_canvas'),
-   #(r'^$', 'fb_canvas',{'app_id':1,'stat_name':'score'}),
+   (r'^$', 'fb_canvas',{'app_id':1,'stat_name':'score'}),
     (r'highscore/(?P<app_id>\d+)/(?P<stat_name>\w+)/$', 'fb_highscore'),
     
 
